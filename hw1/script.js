e04d96382cf111307515b4e17af16ee0f5408a73
yelim0522 <yelim040522@gmail.com>
2026-04-04 23:42:12 +0900

    hw1 과제 제출

--- hw1/script.js
+++ hw1/script.js
@@ -1,6 +1,6 @@
-const cheerButton = document.getElementById('cheerButton');
+const cheerButton = document.getElementById("cheerButton");
 
-cheerButton.addEventListener('click', () => {
-  cheerButton.textContent = '___의 계획 제출하기!';
-  alert('제출이 완료되었습니다!');
+cheerButton.addEventListener("click", () => {
+  cheerButton.textContent = "신예림의 계획 제출하기!";
+  alert("화이팅!!");
 });
