ed73d9d7d8e74271269b352825f6bb740929b2b5
yelim0522 <yelim040522@gmail.com>
2026-04-04 22:11:33 +0900

    i new file
    Esc
    :wq

---
+++ hw1/script.js
@@ -0,0 +1,6 @@
+const cheerButton = document.getElementById('cheerButton');
+
+cheerButton.addEventListener('click', () => {
+  cheerButton.textContent = '___의 계획 제출하기!';
+  alert('제출이 완료되었습니다!');
+});
